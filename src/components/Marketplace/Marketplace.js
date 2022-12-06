864b320601f519778e55685744cd15fb189f1bd1
Seamus Naaykens <snaaykens@gmail.com>
2022-12-06 13:43:38 -0700

    created marketplace jsx and classnames

--- src/components/Marketplace/Marketplace.js
+++ src/components/Marketplace/Marketplace.js
@@ -1,8 +1,14 @@
 import './Marketplace.scss'
+import { Link } from 'react-router-dom'
 
 function Marketplace() {
     return(
-        <></>
+        <div className='marketplace'>
+            <Link className='marketplace__item'>
+            <img className='marketplace__item-image'/>
+            <h3 className='marketplace__item-name'></h3>
+            </Link>
+        </div>
     )
 }
 export default Marketplace
