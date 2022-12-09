4b3d24a9d05dae42e08a02d0ae2d804b438de6e5
Seamus Naaykens <snaaykens@gmail.com>
2022-12-08 17:05:15 -0700

    created the logic to access a single users info and passed it down as a prop

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,14 +2,34 @@ import './Profile.scss'
 import ProfileInfo from '../../components/ProfileInfo/ProfileInfo'
 import Chatbox from '../../components/Chatbox/Chatbox'
 import ProfileMarketplace from '../../components/ProfileMarketplace/ProfileMarketplace'
-
-
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Profile() {
+
+    const { profileId } = useParams
+
+    const passProfileId = profileId || null
+
+    const [ user, setUser ] =useState([])
+
+    useEffect(() => {
+        if (passProfileId === null) {
+            return;
+        }
+        axios.get(`http://localhost:8080/${passProfileId}`)
+        .then(response => {
+            setUser(response.data)
+        }).catch(err => {
+            console.log("Error 404 Bad Request", err)
+        })
+     }, [])
+
     return (
         <main className='profile__page'>
             <div className='profile__page-content-container'>
-                <ProfileInfo />
+                <ProfileInfo userInfo = {user} />
                 <Chatbox />
             </div>
             <ProfileMarketplace />
