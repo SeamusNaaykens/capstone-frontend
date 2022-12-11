906249f764233b27538e5095f5b94a39bd72b6f9
Seamus Naaykens <snaaykens@gmail.com>
2022-12-11 10:33:40 -0700

    created a static chatbox component to represent the working version

--- src/components/Chatbox/Chatbox.js
+++ src/components/Chatbox/Chatbox.js
@@ -1,6 +1,18 @@
+import './Chatbox.scss'
+
 function Chatbox() {
     return(
-        <div></div>
+        <div className="chatbox">
+            <h2 className="chatbox__heading">Start a Conversation</h2>
+            <div className="chatbox__chat-field"></div>
+            <div className='chatbox__input-container'>
+                <input 
+                className="chatbox__input" 
+                type='text'
+                placeholder="Type your message here..."></input>
+                <button className="chatbox__button">Send</button>
+            </div>
+        </div>
     )
 }
 export default Chatbox
